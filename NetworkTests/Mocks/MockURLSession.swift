8a733d4bd3839c88b37e0308d7dc4a994d6ac078
Kingsey.Charles <kingsley108@yahoo.co.uk>
2023-07-20 18:50:50 +0100

    Added files to gitignore and removed unneeded initializer

--- NetworkTests/Mocks/MockURLSession.swift
+++ NetworkTests/Mocks/MockURLSession.swift
@@ -7,12 +7,6 @@ public class MockURLSession: URLSession {
     public var data: Data?
     public var response: URLResponse?
     public var error: Error?
-    
-    public init(data: Data? = nil, response: URLResponse? = nil, error: Error? = nil) {
-        self.data = data
-        self.response = response
-        self.error = error
-    }
 
     public override func dataTask(with request: URLRequest, completionHandler: @escaping CompletionHandler) -> URLSessionDataTask {
         let task = MockURLSessionDataTask()
